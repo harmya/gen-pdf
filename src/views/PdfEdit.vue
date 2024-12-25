a9b74f4b43fe4223af0d7c205b69e6b68d8927aa
harmya <hvbhatt@purdue.edu>
2024-12-25 01:15:54 -0500

    can drag and drop

--- src/views/PdfEdit.vue
+++ src/views/PdfEdit.vue
@@ -3,15 +3,17 @@
   <div class="container">
     <!-- Assets Section -->
     <div class="section section-1">
-      <h2>Assets</h2>
+      <h2>Add Text</h2>
+      <input type="text" />
+      <h2>Add Images</h2>
       <input type="file" multiple @change="handleImageUpload" />
       <div class="assets-preview">
         <div
           class="asset-wrapper"
           v-for="(asset, index) in assets"
           :key="index"
-          draggable="true"
-          @dragstart="ondragstart"
+          draggable
+          @dragstart="ondragstart($event)"
         >
           <img :src="asset" alt="Asset" class="asset" />
         </div>
@@ -19,8 +21,8 @@
     </div>
 
     <!-- Canvas Section -->
-    <div class="section section-2">
-      <canvas id="canvas" ref="canvas" @drop="ondrop" @dragover.prevent> </canvas>
+    <div class="section section-2" @drop="ondrop" @dragover.prevent>
+      <canvas id="canvas" ref="canvas"></canvas>
     </div>
 
     <!-- Save Section -->
@@ -80,30 +82,31 @@
 }
 
 #canvas {
-  border: 1px solid white;
+  border: 1px solid black;
   display: block;
 }
 </style>
 <script lang="ts">
 import * as fabric from 'fabric'
-import { console } from 'inspector'
+import { markRaw } from 'vue'
 
 export default {
+  name: 'PdfEdit',
   data() {
     return {
       assets: [] as string[],
       canvas: null as fabric.Canvas | null,
     }
   },
-
   mounted() {
     const ref = this.$refs.canvas as HTMLCanvasElement
     const section = ref.parentElement as HTMLElement
-
     ref.height = section.clientHeight - 50
     ref.width = section.clientHeight * 0.7069
 
-    this.canvas = new fabric.Canvas(ref)
+    this.canvas = this.canvas = new fabric.Canvas(ref, {
+      selection: true,
+    })
   },
 
   methods: {
@@ -122,12 +125,16 @@ export default {
       }
     },
     ondragstart(event: DragEvent) {
+      if (event.dataTransfer) {
+        event.dataTransfer.dropEffect = 'move'
+        event.dataTransfer.effectAllowed = 'move'
+      }
       const target = event.target as HTMLElement
       const img_src = target.getAttribute('src') as string
       event.dataTransfer?.setData('text/plain', img_src)
     },
 
-    async ondrop(event: DragEvent) {
+    ondrop(event: DragEvent) {
       console.log(event)
       const data = event.dataTransfer?.getData('text/plain')
       if (data) {
@@ -135,8 +142,14 @@ export default {
           img.set({
             left: event.offsetX,
             top: event.offsetY,
+            scaleX: 0.5,
+            scaleY: 0.5,
+            cornerSize: 10,
+            hasBorders: true,
+            hasControls: true,
           })
-          this.canvas?.add(img)
+          this.canvas?.add(markRaw(img))
+          this.canvas?.setActiveObject(img)
         })
       }
     },
